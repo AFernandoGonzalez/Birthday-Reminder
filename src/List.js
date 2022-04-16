cfda3d77550927e86366edf6b597f9e2b9c50e68
Fernando <ferg.developer@gmail.com>
2022-04-16 14:47:44 -0400

    single person can be deleted

--- src/List.js
+++ src/List.js
@@ -1,19 +1,40 @@
-import React from 'react';
+// import React from 'react';
+import React, { useState } from 'react';
+import data from './data';
 
-const List = ({people}) => {
+const List = () => {
+
+  // const {id, age, image } = people
+  const [people, setPeople] = useState(data)
+
+  const remove = (id) => {
+    const newList = people.filter((person) => person.id !== id);
+    // console.log(`removing ${id}`);
+    setPeople(newList);
+  }
+
   return (
     <>
-      {people.map((person)=> {
-        const{id, name, age, image} = person;
-        return <article key={id} className='person'>
-          <img src={image} alt={name} />
-          <div>
-            <h4>{name}</h4>
-            <p>{age} years</p>
-          </div>
-        </article>
-        
+      <h3>{people.length} birthdays today</h3>
+      {people.map((person) => {
+        const { id, name, age, image } = person;
+
+        return (
+          <article key={id} className='person'>
+            <img src={image} alt={name} />
+            <div>
+              <h4>{name}</h4>
+              <p>{age} years</p>
+              <button className='btn' onClick={() => remove(id)} >Delete</button>
+            </div>
+          </article>
+
+
+        )
+
       })}
+
+      <button onClick={() => setPeople([])}>clear all</button>
     </>
   );
 };
